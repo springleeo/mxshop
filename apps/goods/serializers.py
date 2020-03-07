0c1abca7c25b48e0e30ee68c2156cb96fbf7d128
springleeo <springleeo@163.com>
2020-03-07 20:16:22 +0800

    1.详细显示商品的类别外键信息

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from goods.models import Goods
+from goods.models import Goods, GoodsCategory
 
 
 # class GoodsSerializer(serializers.Serializer):
@@ -8,9 +8,21 @@ from goods.models import Goods
 # 	goods_front_image = serializers.ImageField()
 # 	add_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
 
+
+class GoodsCategorySerializer(serializers.ModelSerializer):
+	"""序列化所有category信息"""
+	class Meta:
+		model = GoodsCategory
+		fields = '__all__'
+
+
 class GoodsSerializer(serializers.ModelSerializer):
+	"""序列化所有商品信息"""
 	# 也可以给单个字段做特殊的定义，比如更改了add_time
 	add_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+	# 实例化商品类别对象，虽然我们上面实例化了类别所有信息，但是它只会覆盖当前商品所属类别的信息
+	category = GoodsCategorySerializer()
 
 	class Meta:
 		model = Goods
