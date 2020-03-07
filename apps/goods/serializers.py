727c6525f4f952a8042e1cc1167672b44e318d9e
springleeo <springleeo@163.com>
2020-03-07 19:50:17 +0800

    1.使用django rest framework的ModelSerializer实现商品列表页

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
+from goods.models import Goods
 
 
-class GoodsSerializer(serializers.Serializer):
-	name = serializers.CharField(required=True, max_length=100)
-	click_num = serializers.IntegerField(default=0)
-	goods_front_image = serializers.ImageField()
+# class GoodsSerializer(serializers.Serializer):
+# 	name = serializers.CharField(required=True, max_length=100)
+# 	click_num = serializers.IntegerField(default=0)
+# 	goods_front_image = serializers.ImageField()
+# 	add_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+class GoodsSerializer(serializers.ModelSerializer):
+	# 也可以给单个字段做特殊的定义，比如更改了add_time
 	add_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+	class Meta:
+		model = Goods
+		fields = '__all__'
